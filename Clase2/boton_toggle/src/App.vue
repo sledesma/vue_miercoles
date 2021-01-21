41de15355b781e4f9ce2ad1f7b1807d259e9d832
sebas <sebas@Scorpion.sebas.com>
2021-01-20 21:15:24 -0300

    Final boton_toggle

---
+++ Clase2/boton_toggle/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- 3° Usarlo (con snake-case) -->
+    <boton-toggle name="Clickeame"></boton-toggle>
+
+    <boton-toggle name="Hola"></boton-toggle>
+
+    <boton-toggle name="Adios"></boton-toggle>
+
+  </div>
+</template>
+
+<script>
+import BotonToggle from './components/BotonToggle'; // 1° Importarlo
+
+// 2° Registrar
+export default {
+  name: 'App',
+  components: {
+    BotonToggle
+  }
+}
+</script>
+
+<style>
+#app {
+  padding: 10px;
+}
+</style>
---
+++ Clase2/boton_toggle/src/components/BotonToggle.vue
@@ -0,0 +1,50 @@
+<template>
+	<button v-bind:class="claseBoton" v-on:click="clickBoton">
+		{{ name }}
+	</button>
+</template>
+
+<script>
+export default {
+	methods: {
+		clickBoton() {
+			this.claseBoton = this.sigEstado;
+			this.sigEstado = this.sigEstado === "enter" ? "leave" : "enter";
+		},
+	},
+
+	data() {
+		return {
+			claseBoton: "",
+			sigEstado: "enter",
+		};
+    },
+    
+    props: {
+        name: String
+    }
+};
+</script>
+
+<style scoped>
+button {
+	background: red;
+	padding: 15px;
+	border: 0;
+	color: white;
+	font-weight: bold;
+	font-family: Arial, Helvetica, sans-serif;
+	outline: none;
+	cursor: pointer;
+}
+
+.enter {
+	padding: 20px;
+	transition: padding 200ms;
+}
+
+.leave {
+	padding: 15px;
+	transition: padding 200ms;
+}
+</style>
